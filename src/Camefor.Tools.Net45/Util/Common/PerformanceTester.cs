cd183935ee7438b51ba05271e49d03aff6eedf27
ryhswang <xuehaq@gmail.com>
2021-08-06 15:28:55 +0800

    UPDATE

---
+++ src/Camefor.Tools.Net45/Util/Common/PerformanceTester.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Camefor.Tools.NetCore.Util.Common
+{
+    /// <summary>
+    /// 描   述  ： 性能测试 https://stackoverflow.com/questions/13681664/helper-class-for-performance-tests-using-stopwatch-class#
+    /// 版   本  ： V1.0.0                            
+    /// 创 建 人 ：                                   
+    /// 日    期 ：                         
+    /// 创 建 人 ：                                   
+    /// 创建时间 ：                                   
+    /// 修 改 人 ：                                   
+    /// 修改时间 ：                                   
+    /// 修改描述 ：                                   
+    /// </summary> 
+    public class PerformanceTester : IDisposable
+    {
+        private Stopwatch _stopwatch = new Stopwatch();
+        private Action<TimeSpan> _callback;
+
+        public PerformanceTester()
+        {
+            _stopwatch.Start();
+        }
+
+        public PerformanceTester(Action<TimeSpan> callback) : this()
+        {
+            _callback = callback;
+        }
+
+        public static PerformanceTester Start(Action<TimeSpan> callback)
+        {
+            return new PerformanceTester(callback);
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+            if (_callback != null)
+                _callback(Result);
+        }
+
+        public TimeSpan Result
+        {
+            get { return _stopwatch.Elapsed; }
+        }
+    }
+}
