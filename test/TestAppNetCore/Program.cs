24cbb36eea5f8a1ad0285bcd73efc1a706e66e55
ryhswang <xuehaq@gmail.com>
2021-08-06 15:04:46 +0800

    添加项目文件。

---
+++ test/TestAppNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestAppNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test/TestAppNetFx/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Camefor.Tools.NetCore;
+namespace TestAppNetFx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestTool tool = new TestTool();
+            tool.SayHello();
+        }
+    }
+}
