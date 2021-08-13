58b0a1a4075b1b853ab076544c2df838830ed776
ryhswang <xuehaq@gmail.com>
2021-08-13 09:53:07 +0800

    add  DailyTrigger

---
+++ src/Camefor.Tools.NetCore/Util/Common/DailyTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camefor.Tools.NetCore.Util
+{
+    /// <summary>
+    /// 描   述  ： to trigger an event at a specific time                         
+    /// 版   本  ： V1.0.0                            
+    /// 创 建 人 ： rhyswang                                  
+    /// 日    期 ：                         
+    /// 创 建 人 ：                                   
+    /// 创建时间 ：                                  
+    /// 修 改 人 ：                                   
+    /// 修改时间 ：                                   
+    /// 修改描述 ：                                   
+    /// </summary> 
+    public class DailyTrigger
+    {
+        readonly TimeSpan triggerHour;
+
+        public DailyTrigger(int hour, int minute = 0, int second = 0)
+        {
+            triggerHour = new TimeSpan(hour, minute, second);
+            InitiateAsync();
+        }
+
+        async void InitiateAsync()
+        {
+            while (true)
+            {
+                var triggerTime = DateTime.Today + triggerHour - DateTime.Now;
+                if (triggerTime < TimeSpan.Zero)
+                    triggerTime = triggerTime.Add(new TimeSpan(24, 0, 0));
+                await Task.Delay(triggerTime);
+                OnTimeTriggered?.Invoke();
+            }
+        }
+
+        public event Action OnTimeTriggered;
+
+        /**
+         * 
+         *   var trigger = new DailyTrigger(16); // every day at 4:00pm
+
+    trigger.OnTimeTriggered += () => 
+    {
+        // Whatever
+    };  
+
+    Console.ReadKey();
+         * 
+         * **/
+    }
+}
