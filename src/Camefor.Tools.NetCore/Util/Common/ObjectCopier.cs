c6843ac3b2248198c083dc6b82185102246e461d
ryhswang <xuehaq@gmail.com>
2021-09-02 09:51:59 +0800

    Add ObjectCopier

---
+++ src/Camefor.Tools.NetCore/Util/Common/ObjectCopier.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Camefor.Tools.NetCore.Util.Common
+{
+    /// <summary>
+    /// 描   述  ： 对象拷贝通用方法                     
+    /// 版   本  ： V1.0.0                            
+    /// 创 建 人 ： rhyswang                                  
+    /// 日    期 ：                         
+    /// 创 建 人 ：                                   
+    /// 创建时间 ：                                  
+    /// 修 改 人 ：                                   
+    /// 修改时间 ：                                   
+    /// 修改描述 ：                                   
+    /// </summary> 
+    public static class ObjectCopier
+    {
+        /// <summary>
+        /// Perform a deep copy of the object via serialization.
+        /// </summary>
+        /// <typeparam name="T">The type of object being copied.</typeparam>
+        /// <param name="source">The object instance to copy.</param>
+        /// <returns>A deep copy of the object.</returns>
+        public static T Clone<T>(T source)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("The type must be serializable.", nameof(source));
+            }
+            // Don't serialize a null object, simply return the default for that object
+            if (ReferenceEquals(source, null)) return default;
+
+            //第一种方式：
+            //using (var stream = new MemoryStream())
+            //{
+            //    IFormatter formatter = new BinaryFormatter();
+            //    formatter.Serialize(stream, source);
+            //    stream.Seek(0, SeekOrigin.Begin);
+            //    return (T)formatter.Deserialize(stream);
+            //}
+
+            //第二种方式：
+            // initialize inner objects individually
+            // for example in default constructor some list property initialized with some values,
+            // but in 'source' these items are cleaned -
+            // without ObjectCreationHandling.Replace default constructor values will be added to result
+            var deserializeSettings = new JsonSerializerSettings { ObjectCreationHandling = ObjectCreationHandling.Replace };
+            return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(source), deserializeSettings);
+        }
+    }
+}
