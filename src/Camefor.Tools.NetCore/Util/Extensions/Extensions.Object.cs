07476b7c164b5234d399971fe765bc41e5b04dd1
ryhswang <xuehaq@gmail.com>
2024-10-11 09:48:20 +0800

    Add GetChangedFields

---
+++ src/Camefor.Tools.NetCore/Util/Extensions/Extensions.Object.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camefor.Tools.NetCore.Util
+{
+
+    /// <summary>
+    /// 特殊扩展
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 获取两个实体字段参数不一致的字段集合
+        /// 增加字段的注释显示
+        /// </summary>
+        /// <param name="original"></param>
+        /// <param name="updated"></param>
+        /// <returns></returns>
+        public static List<Tuple<string, string, object, object>> GetChangedFieldsWithComment<T>(object original, object updated)
+        {
+            var result = new List<Tuple<string, string, object, object>>();
+
+            var comment = XmlDocumentHelper.GetPropCommentByClassType<T>();
+
+
+            Type type = original.GetType();
+            PropertyInfo[] properties = type.GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                if (!property.CanRead)
+                {
+                    continue;
+                }
+
+                object originalValue = property.GetValue(original);
+                object updatedValue = property.GetValue(updated);
+
+                if (!Equals(originalValue, updatedValue))
+                {
+                    var propertyName = property.Name;
+                    var propertyComment = propertyName;
+                    if (comment.TryGetValue(propertyName, out string propertyDocumentation))
+                    {
+                        propertyComment = propertyDocumentation ?? string.Empty;
+                    }
+                    else
+                    {
+                        propertyComment = propertyName;
+                    }
+
+                    if (propertyName != null && propertyComment != null)
+                    {
+                        result.Add(Tuple.Create(propertyName, propertyComment, originalValue, updatedValue));
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/Camefor.Tools.NetCore/Util/XML/XmlDocumentHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Camefor.Tools.NetCore.Util
+{
+
+    /// <summary>
+    /// 解析xml
+    /// </summary>
+    public class XmlDocumentHelper
+    {
+
+        /// <summary>
+        /// read comments from xml document
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Dictionary<string, string> GetPropCommentByClassType<T>()
+        {
+            var dicComments = new Dictionary<string, string>();//save comments data, the dictionary, key is class name or prop name. and value is comment
+            Type type = typeof(T);
+            PropertyInfo[] properties = type.GetProperties();
+
+            var path = type.Assembly.Location;
+            var file = new FileInfo(path);
+            var xmlPath = type.Assembly.Location.Replace(file.Extension, ".xml");
+
+            if (!File.Exists(xmlPath))
+            {
+                throw new FileNotFoundException($"{file.Name} xml document not found,please configure project propperty,Generate documentation");
+            }
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlPath);
+
+            // get class comment
+            XmlNode classNode = xmlDoc.SelectSingleNode($"/doc/members/member[@name='T:{type.FullName}']");
+            string classComment = classNode?.SelectSingleNode("summary")?.InnerText.Trim();
+            dicComments.Add(type.FullName ?? "classFullName", classComment);
+
+            //get property or field comment
+            foreach (var property in properties)
+            {
+                var propertyName = property.Name;
+                //var memberName = property.MemberType switch
+                //{
+                //    MemberTypes.Property => $"P:{property?.DeclaringType?.FullName}.{property?.Name}",
+                //    MemberTypes.Field => $"F:{property?.DeclaringType?.FullName}.{property?.Name}",
+                //    _ => throw new NotSupportedException($"Unsupported member type: {property.MemberType}")
+                //};
+                var memberName = string.Empty;
+                switch (property.MemberType)
+                {
+                    case MemberTypes.Field:
+                        memberName = $"F:{property?.DeclaringType?.FullName}.{property?.Name}";
+                        break;
+                    case MemberTypes.Property:
+                        memberName = $"P:{property?.DeclaringType?.FullName}.{property?.Name}";
+                        break;
+                    default:
+                        throw new NotSupportedException($"Unsupported member type: {property.MemberType}");
+                }
+                var summaryNode = xmlDoc.SelectSingleNode($"//member[starts-with(@name, '{memberName}')]/summary");
+                var propertyDocumentation = summaryNode?.InnerText.Trim();
+                dicComments.Add(propertyName, propertyDocumentation);
+            }
+
+            return dicComments;
+        }
+    }
+}
